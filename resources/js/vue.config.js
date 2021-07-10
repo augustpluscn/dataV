c6a7737bd9e210501c7f9381bb5202563f1eb4fc
august <1@augustor.com>
2021-07-10 14:35:05 +0800

    基础环境

---
+++ resources/js/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  outputDir: '../../public',
+  indexPath: process.env.NODE_ENV === 'production'
+    ? '../resources/views/app.blade.php'
+    : 'index.html',
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = '博格'
+        return args
+      })
+  }
+};
